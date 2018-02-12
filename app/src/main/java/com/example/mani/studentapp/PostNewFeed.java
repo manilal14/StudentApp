49a892d9c30308c05361ee6d4b6947ab4022e4bc
mani <manihereatcusat@gmail.com>
2018-02-12 16:17:15 +0530

    Image can be selected.

--- app/src/main/java/com/example/mani/studentapp/PostNewFeed.java
+++ app/src/main/java/com/example/mani/studentapp/PostNewFeed.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.theartofdev.edmodo.cropper.CropImage;
@@ -13,6 +15,8 @@ import com.theartofdev.edmodo.cropper.CropImageView;
 public class PostNewFeed extends AppCompatActivity {
 
     private ImageButton mSelectImage;
+    private EditText mTitle,mDescription;
+    private Button mSubmitbtn;
     private static final int GALLARY_REQUEST = 1;
 
     @Override
@@ -21,6 +25,10 @@ public class PostNewFeed extends AppCompatActivity {
         setContentView(R.layout.activity_post_new_feed);
 
         mSelectImage = findViewById(R.id.post_new_feed_imageview);
+        mTitle        = findViewById(R.id.post_new_feed_title);
+        mDescription  = findViewById(R.id.post_new_feed_describtion);
+        mSubmitbtn    = findViewById(R.id.post_new_feed_submit_btn);
+
         mSelectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,6 +37,15 @@ public class PostNewFeed extends AppCompatActivity {
                     startActivityForResult(gallaryIntent,GALLARY_REQUEST);
             }
         });
+
+        mSubmitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendDataToDatabase();
+            }
+        });
+
+
     }
 
     @Override
@@ -46,6 +63,7 @@ public class PostNewFeed extends AppCompatActivity {
                     .start(this);
         }
 
+
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
 
@@ -57,5 +75,16 @@ public class PostNewFeed extends AppCompatActivity {
                 Exception error = result.getError();
             }
         }
+    }
+
+    /*
+        In this method, if title and description field are not empty the
+        data will be send to database.
+        And then updated information will be fetched and will be shown in News Feed.
+     */
+    private void sendDataToDatabase()
+    {
+        //Todo 1. send data to database
+        //Todo 2. Fetch from database
     }
 }

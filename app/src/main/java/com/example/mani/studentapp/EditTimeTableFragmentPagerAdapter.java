866103ae5515235c91f14d98f437681056f438e7
mani <manihereatcusat@gmail.com>
2018-03-29 03:26:21 +0530

    Fragments for EditTimeTable are added.

---
+++ app/src/main/java/com/example/mani/studentapp/EditTimeTableFragmentPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.example.mani.studentapp;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class EditTimeTableFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public EditTimeTableFragmentPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0: return new MondayFragment();
+            case 1: return new TuesdayFragment();
+            case 2: return new WednesdayFragment();
+            case 3: return new ThrusdayFragment();
+            case 4: return new FridayFragment();
+
+        }
+
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position){
+            case 0: return "Monday";
+            case 1: return "Tuesday";
+            case 2: return "Wednesday";
+            case 3: return "Thrusday";
+            case 4: return "Friday";
+
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 5;
+    }
+}

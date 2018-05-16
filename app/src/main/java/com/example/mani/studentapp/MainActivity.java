2e810ca1beeba950694ba4c235dff991d8fd7f87
mani <manihereatcusat@gmail.com>
2018-05-16 23:26:49 +0530

    Phase2 starts. Package are made.

--- app/src/main/java/com/example/mani/studentapp/MainActivity.java
+++ app/src/main/java/com/example/mani/studentapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.mani.studentapp.NewsRelaled.NewsFeed;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView mLogin;

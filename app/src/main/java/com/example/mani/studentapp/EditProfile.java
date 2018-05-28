21cbd1f64401fdb09ee48b96f02fef969cff089a
mani <manihereatcusat@gmail.com>
2018-05-29 02:36:14 +0530

    Design of Edit Profile is over.

--- app/src/main/java/com/example/mani/studentapp/EditProfile.java
+++ app/src/main/java/com/example/mani/studentapp/EditProfile.java
@@ -2,6 +2,9 @@ package com.example.mani.studentapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class EditProfile extends AppCompatActivity {
 
@@ -14,5 +17,25 @@ public class EditProfile extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_edit_profile,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if(id == R.id.menu_save){
+
+            Toast.makeText(EditProfile.this,"will implement",Toast.LENGTH_SHORT).show();
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
